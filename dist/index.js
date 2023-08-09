dd24cce7bacdc4737268f4ca58f2316a6d649a67
LanzoInc <olanzovc@gmail.com>
2023-08-09 14:18:48 -0400

    first commit

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,3 @@
-"use strict";
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -8,18 +7,13 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const axios_1 = __importDefault(require("axios"));
-const express_1 = __importDefault(require("express"));
+import fetch from "node-fetch";
+import express from "express";
 /**
  * Port to listen on: localhost:PORT || localhost:8080
  */
-const SERVER_PORT = 8080;
-const HOST_DOMAIN = "roblox.com";
-const app = (0, express_1.default)();
+const SERVER_PORT = process.env.SERVER_PORT || 80;
+const app = express();
 /**
  * Middleware to not capture request made to /favicon.ico
  */
@@ -37,46 +31,90 @@ app.use(noFavicon);
  * Captures all requests.
  */
 app.all("*", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    /**
-     * Get the subdomains of the request, if none, default to www
-     */
-    const subdomain = req.subdomains.length > 0 ? req.subdomains.join(".") : "www";
-    const uri = "https://" + subdomain + "." + HOST_DOMAIN + req.originalUrl;
-    /**
-     * Get the requests method, POST | PATCH | GET, etc... If not then default to GET (which probably should never happen)
-     */
-    const m = req.method || "GET";
-    /**
-     * Make the api request using axios
-     */
-    yield axios_1.default
-        .request({
-        method: m,
-        url: uri,
-        headers: req.headers,
-    })
-        .then((result) => {
-        /**
-         * Send the data received
-         */
-        res.status(result.status).send(result.data);
+    fetch("https://catalog.roblox.com/v1/categories")
+        .then((r) => {
+        res.status(r.status).send(r.body);
     })
         .catch((err) => {
-        if (err.response) {
-            /**
-             * Response error
-             */
-            return res.status(err.response.status).send(err.response.data);
-        }
-        /**
-         * Something went wrong on the proxies end
-         */
-        console.warn("Internal Error.");
-        res.status(400).json({ success: false, message: err.toString() });
+        console.log(err);
     });
 }));
-/**
- * Listen on PORT
- */
 app.listen(SERVER_PORT);
+console.log(`LISTENING ON PORT: ${SERVER_PORT}`);
+// /**
+//  * Port to listen on: localhost:PORT || localhost:8080
+//  */
+// const SERVER_PORT = 8080;
+// const HOST_DOMAIN = "roblox.com";
+// const app = express();
+// /**
+//  * Middleware to not capture request made to /favicon.ico
+//  */
+// const noFavicon = (
+//   req: express.Request,
+//   res: express.Response,
+//   next: express.NextFunction
+// ) => {
+//   if (req.originalUrl.includes("favicon.ico")) {
+//     return res.status(204).end();
+//   }
+//   next();
+// };
+// /**
+//  * Use no favicon middleware
+//  */
+// app.use(noFavicon);
+// /**
+//  * Captures all requests.
+//  */
+// app.all("*", async (req, res) => {
+//   /**
+//    * Get the subdomains of the request, if none, default to www
+//    */
+//   // console.log(req.subdomains);
+//   // res.send("Hello");
+//   // return;
+//   const subdomain: string =
+//     req.subdomains.length > 0 ? req.subdomains.join(".") : "www";
+//   const uri: string =
+//     "https://" + subdomain + "." + HOST_DOMAIN + req.originalUrl;
+//   console.log("endpoint hit : " + req.originalUrl + " || forward => " + uri);
+//   /**
+//    * Get the requests method, POST | PATCH | GET, etc... If not then default to GET (which probably should never happen)
+//    */
+//   const m = req.method || "GET";
+//   /**
+//    * Make the api request using axios
+//    */
+//   await axios
+//     .request({
+//       method: m,
+//       url: uri,
+//       headers: req.headers,
+//     })
+//     .then((result) => {
+//       /**
+//        * Send the data received
+//        */
+//       res.status(result.status).send(result.data);
+//     })
+//     .catch((err) => {
+//       if (err.response) {
+//         /**
+//          * Response error
+//          */
+//         return res.status(err.response.status).send(err.response.data);
+//       }
+//       /**
+//        * Something went wrong on the proxies end
+//        */
+//       console.warn("Internal Error.", err);
+//       res.status(400).json({ success: false, message: err.toString() });
+//     });
+// });
+// /**
+//  * Listen on PORT
+//  */
+// app.listen(SERVER_PORT);
+// console.log("LISTENING ON PORT", SERVER_PORT);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,11 @@
-import axios from "axios";
+import http from "http";
+import fetch from "node-fetch";
 import express from "express";
 
 /**
  * Port to listen on: localhost:PORT || localhost:8080
  */
-const SERVER_PORT = 8080;
-
-const HOST_DOMAIN = "roblox.com";
+const SERVER_PORT = process.env.SERVER_PORT || 80;
 
 const app = express();
 /**
@@ -32,50 +31,14 @@ app.use(noFavicon);
  * Captures all requests.
  */
 app.all("*", async (req, res) => {
-  /**
-   * Get the subdomains of the request, if none, default to www
-   */
-  const subdomain: string =
-    req.subdomains.length > 0 ? req.subdomains.join(".") : "www";
-  const uri: string =
-    "https://" + subdomain + "." + HOST_DOMAIN + req.originalUrl;
-
-  /**
-   * Get the requests method, POST | PATCH | GET, etc... If not then default to GET (which probably should never happen)
-   */
-  const m = req.method || "GET";
-
-  /**
-   * Make the api request using axios
-   */
-  await axios
-    .request({
-      method: m,
-      url: uri,
-      headers: req.headers,
-    })
-    .then((result) => {
-      /**
-       * Send the data received
-       */
-      res.status(result.status).send(result.data);
+  fetch("https://catalog.roblox.com/v1/categories")
+    .then((r) => {
+      res.status(r.status).send(r.body);
     })
     .catch((err) => {
-      if (err.response) {
-        /**
-         * Response error
-         */
-        return res.status(err.response.status).send(err.response.data);
-      }
-      /**
-       * Something went wrong on the proxies end
-       */
-      console.warn("Internal Error.");
-      res.status(400).json({ success: false, message: err.toString() });
+      console.log(err);
     });
 });
 
-/**
- * Listen on PORT
- */
+console.log(`LISTENING ON PORT: ${SERVER_PORT}`);
 app.listen(SERVER_PORT);
